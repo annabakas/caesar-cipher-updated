7fe5e6cc503bbe99489a7764e6b8c395ca2ebc06
Anna Bakas <abakas@luc.edu>
2020-09-26 16:52:25 -0500

    started making tests

--- ceaser_tests.cc
+++ ceaser_tests.cc
@@ -4,6 +4,16 @@
 
 #include "ceaser.hh"
 
+TEST(StringCompareTest, CorrectCompare){
+	char string1[12] = "Hello";
+	char string2[12] = "Hello";
+	char string3[12] = "DONUTS";
+	char string4[12] = "DONUTS";
+	ASSERT_EQ(0, stringCompare(string1, string2));
+	ASSERT_EQ(0, stringCompare(string3, string4));
+	
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
