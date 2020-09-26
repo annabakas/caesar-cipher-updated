e0d8b56ea1f38a3f999fd017c6ffde7b229d6fa6
Anna Bakas <abakas@luc.edu>
2020-09-26 16:16:41 -0500

    ceaser.hh file

---
+++ ceaser.hh
@@ -0,0 +1,12 @@
+// ceaser.hh
+// This header ensures C linkage is enforced when running C++ unit tessts on C code.
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "ceaser.h"
+
+#ifdef __cplusplus
+}
+#endif
