72444782f77c953beee2c01cfb404d2fb9d04779
Anna Bakas <abakas@luc.edu>
2020-09-26 16:37:36 -0500

    moved main here

---
+++ ceaser_app.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "ceaser.h"
+#define MAX_WORD_LENGTH 330
+int main(){
+	FILE *fp;
+	fp = fopen("shifts.txt", "w");
+
+	char buffer[MAX_WORD_LENGTH];
+	int count = 0, max = shifts[0];
+
+	sort();
+	int n;
+	while((fgets(buffer, MAX_WORD_LENGTH, stdin)) != NULL){
+		count++;
+		printf("%d\n", count);
+		split(buffer);
+
+		printf("\nBest Shift: %d\n", bestShift());
+		fprintf(fp, "%d\n", bestShift());
+
+		clearShifts();
+		if(count == 50){
+			break;
+		}
+
+	}
+	
+	fclose(fp);
+	return 0;
+}
